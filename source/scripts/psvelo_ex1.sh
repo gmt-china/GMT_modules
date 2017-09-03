fe84a33b291af83f46ecb5e51f607105ea062286
Dongdong Tian <seisman.info@gmail.com>
2017-09-03 17:31:55 +0800

    update script

--- source/scripts/psvelo_ex1.sh
+++ source/scripts/psvelo_ex1.sh
@@ -2,8 +2,8 @@
 ps=psvelo_ex1.ps
 
 # 左边三个符号
-gmt psvelo << EOF -Xc -JM15c -R241/242/34/35 -B0.5 -BWeSn+tpsvelo \
-    -Sw3c/1.e7 -W1p -G60 -E210 -D2 -P -K > $ps
+gmt psvelo -Xc -JM15c -R241/242/34/35 -B0.5 -BWeSn+tpsvelo \
+    -Sw3c/1.e7 -W1p -G60 -E210 -D2 -P -K > $ps << EOF
 # lon     lat    spin(rad/yr) spin_sigma (rad/yr)
 241.2 34.2  5.65E-08 1.17E-08
 241.2 34.5 -4.85E-08 1.85E-08
@@ -11,7 +11,7 @@ gmt psvelo << EOF -Xc -JM15c -R241/242/34/35 -B0.5 -BWeSn+tpsvelo \
 EOF
 
 # 右边三个符号
-gmt psvelo -J -R -Sw3c/1.e7 -W1p -D2 -O -K << EOF >> $ps
+gmt psvelo -J -R -Sw3c/1.e7 -W1p -D2 -O -K >> $ps << EOF
 # lon     lat    spin(rad/yr) spin_sigma (rad/yr)
 241.7 34.2  5.65E-08 1.17E-08
 241.7 34.5 -4.85E-08 1.85E-08
