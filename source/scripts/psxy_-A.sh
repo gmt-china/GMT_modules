3d03dccccefd0eeba0108c0e1839236142014a72
Dongdong Tian <seisman.info@gmail.com>
2018-04-06 09:55:19 +0800

    psxy -A: use thicker pen

--- source/scripts/psxy_-A.sh
+++ source/scripts/psxy_-A.sh
@@ -5,19 +5,19 @@ PS=psxy_-A.ps
 p1="30 -50"
 p2="200 45"
 gmt psbasemap -J$J -R$R -Bx60 -By30 -K > $PS
-gmt psxy -J$J -R$R -K -O >> $PS << EOF
+gmt psxy -J$J -R$R -W2p -K -O >> $PS << EOF
 $p1
 $p2
 EOF
-gmt psxy -J$J -R$R -A -K -O -Wred >> $PS << EOF
+gmt psxy -J$J -R$R -A -K -O -W2p,red >> $PS << EOF
 $p1
 $p2
 EOF
-gmt psxy -J$J -R$R -Ap -K -O -Wblue >> $PS << EOF
+gmt psxy -J$J -R$R -Ap -K -O -W2p,blue >> $PS << EOF
 $p1
 $p2
 EOF
-gmt psxy -J$J -R$R -Am -K -O -Wyellow >> $PS << EOF
+gmt psxy -J$J -R$R -Am -K -O -W2p,yellow >> $PS << EOF
 $p1
 $p2
 EOF
