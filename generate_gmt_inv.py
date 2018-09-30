15ede3f9e48085ea67b3d89c5a1fe4787bb190b7
Dongdong Tian <seisman.info@gmail.com>
2018-09-30 00:11:29 -0400

    add polespotter

--- generate_gmt_inv.py
+++ generate_gmt_inv.py
@@ -56,7 +56,7 @@ supplements = {
     'segy'  : ['pssegy', 'pssegyz', 'segy2grd'],
     'spotter': ['backtracker', 'gmtpmodeler', 'grdpmodeler', 'grdspotter',
                 'originator', 'grdrotater', 'hotspotter', 'rotconverter',
-                'rotsmoother'],
+                'rotsmoother', 'polespotter'],
     'x2sys' :  ['x2sys_cross', 'x2sys_init', 'x2sys_put', 'x2sys_datalist',
                 'x2sys_list', 'x2sys_report', 'x2sys_binlist', 'x2sys_get',
                 'x2sys_merge', 'x2sys_solve'],
